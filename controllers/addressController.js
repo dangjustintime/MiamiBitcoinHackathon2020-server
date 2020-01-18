eb0ce7a98853679ddefca9614d5d7967e3009b40
dangjustintime <dangjustintime@gmail.com>
2020-01-18 16:57:00 -0500

    added address api

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // get address
 router.get('/:address', async (request, response) => {
 	try {
-		const blockCypherResponse = await fetch(`https://api.blockcypher.com/v1/btc/main/addrs/${request.params.address}`);
-		const blockCypherResponseJson = await blockCypherResponse.json();
-		response.json(blockCypherResponseJson);
+		const blockCypherResponse = await axios.get(`https://api.blockcypher.com/v1/btc/main/addrs/${request.params.address}/full?before=300000`);
+		// const blockCypherResponseJson = await blockCypherResponse;
+		console.log('&&&&&&', blockCypherResponse.data);
+		response.json(blockCypherResponse.data);
 	} catch (e) {
 		console.log(e);
 	}
